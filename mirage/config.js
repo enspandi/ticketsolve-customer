4a61a9aa318c9def9b85bc9debedf623dbc83cf0
Andreas Minnich <andreas.minnich@gmail.com>
2019-01-29 17:16:49 +0100

    Extend server-side filter to match for fullname

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,13 @@
 import { isBlank } from '@ember/utils';
 
+function filterByFullName(records, name) {
+  name = name.toLowerCase();
+  return records.filter(({ firstName = '', lastName = '' }) => {
+    let fullName = `${firstName} ${lastName}`.toLowerCase();
+    return fullName.includes(name);
+  });
+}
+
 function filterByKeyword(records, attrs, keyword) {
   keyword = keyword.toLowerCase();
   return records.filter((record) => {
@@ -14,14 +22,17 @@ export default function() {
 
   this.get('/customers', (schema, request) => {
     let keyword = request.queryParams['filter'];
+    let allCustomers = schema.db.customers;
     if (!isBlank(keyword)) {
-      return filterByKeyword(
-        schema.db.customers,
+      let resultByFullName = filterByFullName(allCustomers, keyword);
+      let resultByKeyword  = filterByKeyword(
+        allCustomers,
         ['firstName', 'lastName', 'email', 'phone', 'mobile'],
         keyword
       );
+      return [...new Set(resultByFullName.concat(resultByKeyword))];
     }
-    return schema.db.customers;
+    return allCustomers;
   });
   this.post('/customers');
   this.put('/customers/:id');
